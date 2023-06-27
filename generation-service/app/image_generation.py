9b88cf2d67778252e0c88f1e236a62c87883dc9c
myunbongs <myunbongs@naver.com>
2023-06-28 07:04:57 +0900

    deploy: 🚀 docker-compose 서비스 추가 및 오류 수정 (#8)

--- generation-service/app/image_generation.py
+++ generation-service/app/image_generation.py
@@ -1,5 +1,5 @@
 from pydantic import BaseModel
-from diffusers import StableDiffusionPipeline, StableDiffusionImg2ImgPipeline
+from diffusers import StableDiffusionPipeline
 import torch
 from PIL import Image
 import random
@@ -22,18 +22,6 @@ class Txt2img(BaseModel):
     number_of_imgs: Optional[int] = 1 
     imgs : Optional[str] = []
     seeds : Optional[int] = []
-
-# class Img2img(BaseModel): 
-#     model: Optional[str]
-#     init_image_num: Optional[int]
-#     prompt: Optional[str] 
-#     negative_prompt: Optional[str] 
-#     num_inference_steps: Optional[int] = 30
-#     guidance_scale: Optional[int] = 7.5 
-#     strength: Optional[float] = 0.8
-#     number_of_imgs: Optional[int] = 1
-#     imgs : Optional[str] = []
-#     seeds : Optional[int] = []
 
 def making_cover_stable_diffusion_txt2img(txt2img: Txt2img):
 
@@ -63,37 +51,6 @@ def making_cover_stable_diffusion_txt2img(txt2img: Txt2img):
 
     return txt2img
 
-# def making_cover_stable_diffusion_img2img(img2img: Img2img):
-
-#     total_init_imgs_num = len(os.listdir('./app/init_images/'))
-#     init_img = select_init_image(img2img.init_image_num, total_init_imgs_num)
-
-#     model = select_model(img2img.model)
-
-#     generator = [torch.Generator(device="cuda").manual_seed(0) for i in range(img2img.number_of_imgs)]
-#     img2img.seeds = [generator[i].seed() for i in range(img2img.number_of_imgs)]
-
-#     pipe = StableDiffusionImg2ImgPipeline.from_pretrained(model, torch_dtype=torch.float16).to(
-#         "cuda"
-#     )
-    
-#     pipe.enable_xformers_memory_efficient_attention()
-
-#     with torch.inference_mode():
-#         imgs = pipe(prompt=img2img.prompt, negative_prompt=img2img.negative_prompt,
-#                 image=init_img, num_inference_steps=img2img.num_inference_steps, 
-#                 num_images_per_prompt=img2img.number_of_imgs, strength = img2img.strength, guidance_scale=img2img.guidance_scale, 
-#                 generator=generator).images   
-
-#         for i, img in enumerate(imgs):
-#             if not os.path.exists('./app/images'):
-#                 os.makedirs('./app/images')
-#             file_name = "./app/images/{}_{}_{}.png".format(img2img.prompt, img2img.seeds[i], i)
-#             img.save(file_name)
-#             img2img.imgs.append(image2string(img))
-#     return img2img
-
-
 def select_model(model_name):
     if model_name == "stable-diffusion":
         model = "runwayml/stable-diffusion-v1-5"
@@ -104,9 +61,3 @@ def select_model(model_name):
     else:
         raise HTTPException(status_code=404, detail="Model not found")
     return model
-
-# def select_init_image(init_image_num, total_num):
-#     for i in range(total_num):
-#         if init_image_num == i:
-#             init_image = Image.open('./app/init_images/{}.jpg'.format(i)).convert("RGB")
-#     return init_image
--- generation-service/app/main.py
+++ generation-service/app/main.py
@@ -5,7 +5,7 @@ from fastapi.middleware.cors import CORSMiddleware
 
 from starlette.responses import JSONResponse
 
-from .image_generate import Txt2img, Img2img, making_cover_stable_diffusion_txt2img, making_cover_stable_diffusion_img2img
+from .image_generation import Txt2img, making_cover_stable_diffusion_txt2img
 
 from PIL import Image
 
@@ -25,14 +25,6 @@ app.add_middleware(
 async def making_cover_txt2img(txt2img: Txt2img):
     global results
     results = making_cover_stable_diffusion_txt2img(txt2img)
-
-    converted_results = jsonable_encoder(results)
-    return JSONResponse(content=converted_results)
-
-@app.post("/img2img")
-async def making_cover_img2img(img2img: Img2img):
-    global results
-    results = making_cover_stable_diffusion_img2img(img2img)
 
     converted_results = jsonable_encoder(results)
     return JSONResponse(content=converted_results)
