04f1e127ba2f5480aae52d50f2dd50994ba21194
newoldname <thjbjs@outlook.com>
2023-06-28 17:00:56 +0900

    feat: ✨ 로컬 DB 연결 성공 (#9)

--- user-service/src/app.module.ts
+++ user-service/src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeORMConfig } from './configs/typeorm.config';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot(typeORMConfig),
+    ConfigModule.forRoot({
+      cache: true,
+      isGlobal: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
